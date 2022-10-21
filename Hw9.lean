52383eb7550ef55783452eee7d1d5d03c9401722
jackcporter <111578020+jackcporter@users.noreply.github.com>
2022-10-21 13:20:53 +0000

    notes

--- Hw9.lean
+++ Hw9.lean
@@ -30,6 +30,34 @@ example (h : X = Y) : X ∪ Z = Y ∪ Z := by
   -- or rewrite [h]
   -- rfl
 
+variable (α : Type)
+variable (a : α)
+#reduce @id α a
+
+def f (n : Nat) : Nat := n + 1
+
+#check f
+#eval f 5
+
+def g : Nat → Nat := fun n => 100000^n
+
+#check g
+#eval g 2
+
+#check g ∘ f
+#eval (g ∘ f) 2
+#eval g (f 2)
+
+#eval (f ∘ g) 2
+
+variable (β γ δ : Type)
+
+theorem Assoc (f : α → β)(g : β → γ)(h : γ → δ) : (h ∘ g) ∘ f = h ∘ (g ∘ f) := by
+  apply funext
+  intro a
+  rfl
+  -- rfl if you would just need to write it out to solve the proof with no other information
+
 theorem problem1 : ∅ ∈ 𝒫 X := by
   intro a
   intro h
@@ -43,6 +71,8 @@ theorem problem2 (U : β → Set α) : ∀ b, U b ⊆ BigUnion U := by
 
 theorem problem3 (h : X ⊆ Y) : (X ×ˢ W) ⊆ (Y ×ˢ W) := sorry
 
-theorem problem4 (h : Y ∩ Z = ∅) : Yᶜ ∪ Zᶜ = Univ := sorry 
+theorem problem4 (h : Y ∩ Z = ∅) : Yᶜ ∪ Zᶜ = Univ := by
+  have false := by exact h
+  
 
 theorem problem5 : (X \ Y) ∪ (Y \ X) = (X ∪ Y) \ (X ∩ Y) := sorry 
